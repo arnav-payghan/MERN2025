64e18c1132822bae0b8badd2d002e14e580e37a4
Arnav Payghan <arn.payghan@gmail.com>
2025-07-13 18:51:45 +0530

    Added nodemon

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get(("/api/notes/"), (req,res) => {
+    res.send("You have 1 note(s).")    
+})
+
 app.listen(3000, () => {
     console.log("Server is running on PORT: 3000");
 })
